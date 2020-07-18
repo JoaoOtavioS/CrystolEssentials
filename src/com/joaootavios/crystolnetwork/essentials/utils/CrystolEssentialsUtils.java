c711b9dec630caf2ce811142e06d7936f776ba69
JoaoOtavioS <joaootaviostivi2@gmail.com>
2020-07-18 04:04:00 -0300

    remove sendSpacedStaffMessage (support stringlist)

--- src/com/joaootavios/crystolnetwork/essentials/utils/CrystolEssentialsUtils.java
+++ src/com/joaootavios/crystolnetwork/essentials/utils/CrystolEssentialsUtils.java
@@ -5,26 +5,13 @@ import org.bukkit.Bukkit;
 import rcore.util.TXT;
 import java.util.List;
 
-
 public class CrystolEssentialsUtils {
 
-    public static void sendStaffMessage(String s) {
+    public static void sendStaffMessage(List<String> messages) {
         Bukkit.getScheduler().runTaskAsynchronously(EssentialsPlugin.getPlugin(EssentialsPlugin.class), () -> {
             Bukkit.getOnlinePlayers().forEach((p) -> {
                 if (p.hasPermission("crystolnetwork.staff")) {
-                    p.sendMessage(TXT.parse(s));
-                }
-            });
-        });
-    }
-
-    public static void sendSpacedStaffMessage(String s) {
-        Bukkit.getScheduler().runTaskAsynchronously(EssentialsPlugin.getPlugin(EssentialsPlugin.class), () -> {
-            Bukkit.getOnlinePlayers().forEach((p) -> {
-                if (p.hasPermission("crystolnetwork.staff")) {
-                    p.sendMessage(" ");
-                    p.sendMessage(TXT.parse(s));
-                    p.sendMessage(" ");
+                    p.sendMessage(TXT.parse(String.join("\n", messages)));
                 }
             });
         });
