942b8d414b841d8f42c82870c9113e644d4c5b36
JoaoOtavioS <joaootaviostivi2@gmail.com>
2020-07-20 12:09:30 -0300

    update messages

--- src/com/joaootavios/crystolnetwork/essentials/commands/members/Lanterna.java
+++ src/com/joaootavios/crystolnetwork/essentials/commands/members/Lanterna.java
@@ -1,5 +1,6 @@
 package com.joaootavios.crystolnetwork.essentials.commands.members;
 
+import com.joaootavios.crystolnetwork.essentials.utils.Messages;
 import com.joaootavios.crystolnetwork.essentials.utils.UUIDMeta;
 import org.bukkit.potion.PotionEffect;
 import org.bukkit.potion.PotionEffectType;
@@ -20,10 +21,11 @@ public class Lanterna extends RCommand {
     @Override
     public void perform() {
         if (UUIDMeta.containsData(asPlayer().getUniqueId(), "lanterna")) {
-            sendActionBar("<c>Você desativou o sistema de visão noturna.");
+            sendActionBar(Messages.LANTERNDISABLE.getMessage());
             asPlayer().removePotionEffect(PotionEffectType.NIGHT_VISION);
         } else {
-            sendActionBar("<e>Você ativou o sistema de visão noturna.");
+            sendActionBar(Messages.LANTERNENABLE.getMessage());
+            UUIDMeta.setMetadata(asPlayer().getUniqueId(), "laterna", 0);
             asPlayer().addPotionEffect(new PotionEffect(PotionEffectType.NIGHT_VISION, Integer.MAX_VALUE, 1));
         }
         playSound(asPlayer(), Sound.ENTITY_VILLAGER_YES, 1, 1);
--- src/com/joaootavios/crystolnetwork/essentials/utils/Messages.java
+++ src/com/joaootavios/crystolnetwork/essentials/utils/Messages.java
@@ -6,7 +6,9 @@ public enum Messages {
     ERRORLOCDISABLE("&cLocalização desabilitada."),
     ERRORNOVIP("&cVocê não possuí o VIP necessário para tal ato"),
     ERRORPLAYEROFFLINE("&cJogador inexistente."),
-    ERRORGENERIC("&cUm erro não identificado aconteceu.");
+    ERRORGENERIC("&cUm erro não identificado aconteceu."),
+    LANTERNDISABLE("<c>Você desativou o sistema de visão noturna."),
+    LANTERNENABLE("<e>Você ativou o sistema de visão noturna.");
 
     private final String message;
     Messages(final String message) {
