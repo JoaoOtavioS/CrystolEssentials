b159e4d8abff1d80488da7aa5dfa3e91cf6ee248
JoaoOtavioS <joaootaviostivi2@gmail.com>
2020-07-22 12:37:53 -0300

    add support more messages.

--- src/com/joaootavios/crystolnetwork/essentials/utils/Messages.java
+++ src/com/joaootavios/crystolnetwork/essentials/utils/Messages.java
@@ -6,6 +6,18 @@ public enum Messages {
     ERRORLOCDISABLE("&cLocalização desabilitada."),
     ERRORNOVIP("&cVocê não possuí o VIP necessário para tal ato"),
     ERRORPLAYEROFFLINE("&cJogador inexistente."),
+
+    // Messages - chats
+    GLOBALCHAT("&cO chat global está desativado no momento."),
+    LOCALCHAT("&cO chat local está desativado no momento."),
+    TELLCMD("<c>Utilize: /tell <jogador> <mensagem>."),
+    TELLNOHAVEFRIENDS("<c>Evite enviar mensagens para si mesmo, porque você não tenta arrumar amigos? =P"),
+    TELLRECEIVE("<b>Você recebeu uma mensagem privada!"),
+    TELLNOONLINE("<7>%s<c> não está online ou está com tell desativado."),
+    NOHAVE250COINS("<c>Você não possuí 250 coins, portanto não pode enviar mensagens."),
+    ERRORMUTED("<c>Você está silenciado, portanto não pode utilizar qualquer meio de comunicação."),
+    GLOBALCHATCMD("<c>Use /g <mensagem> para enviar uma mensagem."),
+
     ERRORGENERIC("&cUm erro não identificado aconteceu."),
     LANTERNDISABLE("<c>Você desativou o sistema de visão noturna."),
     LANTERNENABLE("<e>Você ativou o sistema de visão noturna.");
