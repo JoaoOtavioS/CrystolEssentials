ec80ebab4fe905c9107fbb579372366ebffdfa44
JoaoOtavioS <joaootaviostivi2@gmail.com>
2020-08-06 09:50:02 -0300

    simple enum

---
+++ src/com/joaootavios/crystolnetwork/essentials/api/enums/StaffType.java
@@ -0,0 +1,13 @@
+package com.joaootavios.crystolnetwork.essentials.api.enums;
+
+public enum StaffType {
+
+    ajudante,
+    moderador,
+    admin,
+    dev,
+    gerente,
+    master,
+    all;
+
+}
